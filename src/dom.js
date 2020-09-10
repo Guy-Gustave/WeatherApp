5118298be1e6b403d6b6a3486cf37beebe728a29
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-09-10 14:53:01 +0100

    create the fetch function to get data

---
+++ src/dom.js
@@ -0,0 +1,4 @@
+const searchbar = document.getElementById('search_term');
+const searchbutton = document.getElementById('search_button');
+
+export { searchbar, searchbutton };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('hello webpack');
+import { searchbar, searchbutton } from './dom';
+
+const infodis = document.getElementById('desc');
+let key = 'bdc41b55a4c712c6598fce628e1a5254';
+let url = 'https://api.openweathermap.org/data/2.5/weather?q=';
+
+let rdd = {};
+const getweather = async (term) => {
+  const res = await (await fetch(`${url}${term}&appid=${key}`)).json();
+  return res;
+};
+
+const datapros = async (term) => {
+  const data = await getweather(term);
+  console.log(data.weather[0].description);
+  infodis.innerHTML = data.weather[0].description;
+};
+datapros('fes');
+searchbutton.addEventListener('click', (e) => {
+  e.preventDefault();
+  datapros(searchbar.value);
+});
